e02aa3c0f62b21e2ff955617f4fedae2dbf57975
Francimar <francimaralexandredt@gmail.com>
2024-07-18 09:47:24 -0300

    update app tarefa: read tarefa feito

--- tarefa/views.py
+++ tarefa/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 
 from .forms import CreateFormTarefa
-
+from .models import Tarefa
 # Create your views here.
 @login_required
 def homepage(request):
-    return render(request,'homepage.html')
+    tarefa = Tarefa.objects.filter(usuario = request.user)
+    return render(request,'homepage.html',{"contexto":tarefa})
 
 @login_required
 def create_tarfa(request):
